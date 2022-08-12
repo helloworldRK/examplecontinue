dc2a32ee65cd9d2c6b5af0f6f8304a2f3bab265a
helloworldRK <110299413+helloworldRK@users.noreply.github.com>
2022-08-12 16:22:05 +0300

    burc

--- src/artikYil.java
+++ src/artikYil.java
@@ -1,2 +1,29 @@
+import java.util.Scanner;
 public class artikYil {
+    public static void main(String[] args) {
+        //Artık Yıl Nasıl Hesaplanır Genel bir kural olarak, artık yıllar 4 rakamının katı olan yıllardır:1988, 1992, 1996, 2000, 2004, 2008, 2012, 2016, 2020, 2024 gibi.100'ün katı olan yıllardan sadece 400'e kalansız olarak bölünebilenler artık yıldır:
+        int yil, kalan = 0;
+        Scanner input = new Scanner(System.in);
+        System.out.println("Yil Giriniz: ");
+        yil = input.nextInt();
+        if(yil%100==0)
+        {
+            if(yil%400==0)
+            {
+                System.out.print(yil+" Artik yildir");
+            }
+            else
+            {
+                System.out.print(yil+" Artik yil degildir");
+            }
+        }
+        else if(yil%4==0)
+        {
+            System.out.print(yil+" Artik yildir");
+        }
+        else
+        {
+            System.out.print(yil+" Artik yil degildir");
+        }
+    }
 }
--- src/cinZodyagi.java
+++ src/cinZodyagi.java
@@ -1,21 +1,6 @@
 import java.util.Scanner;
 public class cinZodyagi {
-    public static void main(String[] args) {/*Çin zodyağı hesaplanırken kişinin doğum yılının 12 ile bölümünde kalana göre bulunur.
-Doğum Tarihi %12 = 0 ➜ Maymun
-Doğum Tarihi %12 = 1 ➜ Horoz
-Doğum Tarihi %12 = 2 ➜ Köpek
-Doğum Tarihi %12 = 3 ➜ Domuz
-Doğum Tarihi %12 = 4 ➜ Fare
-Doğum Tarihi %12 = 5 ➜ Öküz
-Doğum Tarihi %12 = 6 ➜ Kaplan
-Doğum Tarihi %12 = 7 ➜ Tavşan
-Doğum Tarihi %12 = 8 ➜ Ejderha
-Doğum Tarihi %12 = 9 ➜ Yılan
-Doğum Tarihi %12 = 10 ➜ At
-Doğum Tarihi %12 = 11 ➜ Koyun
-Senaryo
-Doğum Yılınızı Giriniz : 1990
-Çin Zodyağı Burcunuz : At */
+    public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
         System.out.println("Hosgeldiniz ");
         System.out.print("Dogum Yiliniz: ");
